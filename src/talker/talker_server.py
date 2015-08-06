be1b29cf6deb750f410fc6f3e45965fd7a6bed2d
Clint Liddick <clint.liddick@gmail.com>
2015-08-06 10:49:47 -0400

    finish talker tts. remove audio play until resolve data transfer

--- src/talker/talker_server.py
+++ src/talker/talker_server.py
@@ -6,7 +6,7 @@ import rospy
 import actionlib
 import os
 
-from talker.msg import SayAction, SayResult, PlayAction, PlayResult
+from talker.msg import SayAction, SayResult
 
 class SayServer(object):
     def __init__(self):
@@ -15,29 +15,14 @@ class SayServer(object):
 
     def say(self, goal):
         rospy.loginfo('saying "{0}"'.format(goal.text))
-        result = os.system('espeak "{0}"'.format(goal.text))
+        result = os.system('espeak -s 160 "{0}"'.format(goal.text))
         if result == 0:
             self.say_server.set_succeeded(SayResult(True))
         else:
             self.say_server.set_succeeded(SayResult(False))
 
 
-class PlayServer(object):
-    def __init__(self):
-        self.play_server = actionlib.SimpleActionServer('play', PlayAction, self.play, False)
-        self.play_server.start()
-
-    def play(self, goal):
-        rospy.loginfo('playing "{0}"'.format(goal.text))
-        result = os.system('espeak "{0}"'.format(goal.text))
-        if result == 0:
-            self.play_server.set_succeeded(PlayResult(True))
-        else:
-            self.play_server.set_succeeded(PlayResult(False))
-
 if __name__ == '__main__':
     rospy.init_node('talker')
     say_server = SayServer()
-    play_server = PlayServer()
     rospy.spin()
-        
